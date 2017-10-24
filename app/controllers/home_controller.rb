0785c5ed6012fb7ef80514a20d2267ed1ebe0246
pkbanks <pkbanks@gmail.com>
2017-10-24 02:11:35 -0400

    added partials

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,6 @@
 class HomeController < ApplicationController
   def index
   	@movies = Movie.all
+  	@movies_older = Movie.where("release_year < ?", 1995)
   end
-
-  def older_movies
-  	# filter for movies before 1995
-  	@movies = Movie.where("release_date < ?", 1995)
-  end
-
 end
