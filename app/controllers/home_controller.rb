b22ddee9a418360245121d87e8244399d98efd21
pkbanks <pkbanks@gmail.com>
2017-10-24 00:45:17 -0400

    adding views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,10 @@ class HomeController < ApplicationController
   def index
   	@movies = Movie.all
   end
+
+  def older_movies
+  	# filter for movies before 1995
+  	@movies = Movie.where("release_date < ?", 1995)
+  end
+
 end
